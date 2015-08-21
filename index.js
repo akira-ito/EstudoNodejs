eb860111c9c9a65de2ecc9b431c33708845de067
Edson Akira ito <edson.akira21@gmail.com>
2015-08-21 17:03:39 -0300

    Tela de codigo de barra

--- index.js
+++ index.js
@@ -3,15 +3,18 @@ var bodyParser = require('body-parser');
 var app = express();
 var indexRouter = require('./routes/index')(express);
 var trancodeRouter = require('./routes/trancode')(express);
+var codigoBarraRouter = require('./routes/codigoBarra')(express);
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(express.static(__dirname + '/bower_components/'));
+app.use(express.static(__dirname + '/public/'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(indexRouter)
-.use(trancodeRouter);
+.use(trancodeRouter)
+.use('/codigoBarra', codigoBarraRouter);
 
 app.use(function(req, res, next){
 	res.status(404);
---
+++ public/js/jquery.js
@@ -0,0 +1,25 @@
+$( "#vencimento" )
+	.change(function() {
+		var barra = form.barra.value;
+		var linha = form.linha.value;
+
+		if (barra && linha){
+			var vencimento = form.venc.value.split('-');
+			var cur = new Date(vencimento[0], vencimento[1]-1, vencimento[2]);
+			var base = new Date(1997,9,7);
+			var data = Math.ceil((cur.getTime() - base.getTime()) / (1000*60*60*24));
+
+			form.barra.value = form.barra.value.substr(0,5)+data+form.barra.value.substr(9);
+			form.linha.value = form.linha.value.substr(0,33)+data+form.linha.value.substr(37);
+		}else{
+			alert('Gere o codigo de barra primeiro');
+		}
+});
+$('#valor')
+	.keydown(function() {
+		var valor = $('#valor').val().replace(/[^0-9]/g,'');
+		$('#valor').val(valor);
+		var valor10 = ('0000000000'.substr(valor.length-10 || 10) + valor);
+		$('#digitado').val($('#digitado').val().substr(0, 37)+valor10);
+		$('#lido').val($('#lido').val().substr(0,10) + valor10 + $('#lido').val().substr(20));
+});
