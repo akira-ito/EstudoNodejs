8f84229ec2f9482b224c25adee81409a02337306
Edson Akira ito <edson.akira21@gmail.com>
2015-09-01 17:09:21 -0300

    trancode com controller

--- public/js/controllerTrancode.js
+++ public/js/controllerTrancode.js
@@ -2,11 +2,13 @@ var trancodeApp = angular.module('trancode', []);
 
 trancodeApp.controller('controllerPost', function($scope, $http){
 	$scope.parse = function(){
+		$scope.loading = "carregando..."
 		$http.post('/request', this.form).
 	 	success(function(data, status, headers, config) {
 			$scope.form = data.form;
 			$scope.res_envio = data.form.envio;
 			$scope.res_resposta = data.form.resposta;
+			$scope.loading = "";
 		}).
 		error(function(data, status, headers, config) {
 			$scope.error = data;
